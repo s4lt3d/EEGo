fad62c678ec91b4f3d01cf59ad1ceb5ff7cf5c40
Walter Gordy <walter.gordy@gmail.com>
2016-07-05 01:28:25 -0600

    Works with VS Code

--- EEGo.go
+++ EEGo.go
@@ -1,13 +1,16 @@
 package main
 
+import "fmt"
+
 //import "fmt"
 
 func main() {
-    GetServer()
-    GetAdvisor(36)
-    GetPrivateMarket(36)
-    GetPublicMarket(36)
-   // Build(36, 0,0,0,0,0,0,0,33)
-    Explore(36, 30) 
-    //fmt.Println(AdvisorTemplate)
+	GetServer()
+	GetAdvisor(36)
+	GetPrivateMarket(36)
+	GetPublicMarket(36)
+	// Build(36, 0,0,0,0,0,0,0,33)
+
+	Explore(36, 30)
+	fmt.Println(AdvisorTemplate)
 }
